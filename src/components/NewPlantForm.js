1c0f7c85a8a1b6f40c60009489f59cf120bb9981
Terry <incubus8@gmail.com>
2021-06-14 16:47:38 -0400

    1st deliverable done, working on 2nd

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,10 +1,32 @@
 import React from "react";
+import {useState} from 'react'
 
 function NewPlantForm() {
+  // const [name, setName] = useState('')
+  // const [image, setImage] = useState('')
+  // const [price, setPrice] = useState('')
+
+  function handleSubmit(e){
+    e.preventDefault()
+  //   fetch('http://localhost:6001/plants', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       name: name,
+  //       image: image,
+  //       price: price,
+  //     }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((newPlant) => addPlant(newPlant))
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
+      <form onSubmit = {handleSubmit}>
         <input type="text" name="name" placeholder="Plant name" />
         <input type="text" name="image" placeholder="Image URL" />
         <input type="number" name="price" step="0.01" placeholder="Price" />
